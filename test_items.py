f05012a07fe7b194aaac97c8f1a7dee5d555e619
SerBur12 <129140981+SerBur12@users.noreply.github.com>
2023-06-12 15:19:55 +0700

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
 
 
-def test_check_exists(browser):
-    browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
+def test_check_exists_button_add_to_cart(browser):
+
+    button = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
+    assert button, 'Button is missing'
